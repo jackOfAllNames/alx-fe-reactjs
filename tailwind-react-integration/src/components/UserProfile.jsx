7a5bedc9e62e17e6b503d882059f4cacf96e3a66
Olaitan Adedokun <kenzdex@gmail.com>
2025-08-08 23:41:16 +0100

    added responsive stylings

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,10 +1,10 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-w-sm my-10 md:my-20">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-36 h-36 mx-auto"
+        className="rounded-full w-36 h-36 mx-auto w-24 h-24 md:w-36 md:h-36"
       />
       <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
       <p className="text-gray-600 text-base">
